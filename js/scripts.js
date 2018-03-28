61f4c6b683d203489d33995eb80f69acf7a0856c
Brian Henry and CJ Rhone <cj@>
2018-03-28 14:59:25 -0700

    Add split and slice functionality to inputted words

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,14 @@ $(document).ready(function() {
   $("form#learnPigLatin").submit(function(event) {
     event.preventDefault();
     var translate = $("input#translate").val();
-    var translateSplit = translate.toLowerCase().split("");
+    var translateSplit = translate.toLowerCase().split("").slice(1,50);
+    alert("I have split and sliced!")
+    alert(translateSplit)
     translateSplit.push("m", "a", "y");
-    console.log(translateSplit.slice(1,50));
+    // console.log(translateSplit.slice(1,50));
+    // translateJoin = translateSplit.join('');
+
+    console.log(translateSplit);
 
 });
 });
