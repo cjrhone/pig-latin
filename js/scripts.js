3a78e5c0cfcedd89d134160574b966a0448186fd
Brian Henry and CJ Rhone <brian@>
2018-03-28 16:30:55 -0700

    Add dancing pig, learned css styling

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,7 @@ $(document).ready(function() {
     var translate = $("input#translate").val();
     var translateSplit = translate.toLowerCase().split("").slice(1,50);
 
-    translateSplit.push("m", "a", "y");
-    // console.log(translateSplit.slice(1,50));
-    // translateJoin = translateSplit.join('');
+    translateSplit.push("w", "a", "y");
 
     var result = translateSplit;
 
@@ -17,4 +15,27 @@ $(document).ready(function() {
 
 
 });
+});
+
+
+
+$(document).ready(function(e) {
+    var width = $(document).width();
+
+    function goRight() {
+        $("#animate").animate({
+        left: 200
+      }, 1000, function() {
+         setTimeout(goLeft, 20);
+      });
+    }
+    function goLeft() {
+        $("#animate").animate({
+        left: 0
+      }, 1000, function() {
+         setTimeout(goRight, 20);
+      });
+    }
+
+    setTimeout(goRight, 20);
 });
