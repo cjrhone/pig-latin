b42a1d818353dbea4e695ef23f37d67359f1ceec
Brian Henry and CJ Rhone <brian@>
2018-03-28 15:43:26 -0700

    Add display to results

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,17 @@ $(document).ready(function() {
     event.preventDefault();
     var translate = $("input#translate").val();
     var translateSplit = translate.toLowerCase().split("").slice(1,50);
-    alert("I have split and sliced!")
-    alert(translateSplit)
+
     translateSplit.push("m", "a", "y");
     // console.log(translateSplit.slice(1,50));
     // translateJoin = translateSplit.join('');
 
-    console.log(translateSplit);
+    var result = translateSplit;
+
+    $("#result").show();
+    $(".finalResult").text(result.join(''));
+
+
 
 });
 });
